f27c5948a22362db3cead1805f078b8736d5570a
01746278199 <rakibahmedra971162@gmail.com>
2023-12-24 17:20:28 +0600

    first commit

---
+++ Variables/variable.js
@@ -0,0 +1,17 @@
+const accountId = 125875
+let accountEmail = 'rakib@gmail.com'
+var accountState = 'test'
+accountCity = 'dhake'
+var accountState = 'final'
+let accountPassword;
+/* 
+we use to let and const 
+cause let and const is block scope
+let:= let is a block scope  . it can reasain but not allowed to redeclared.
+const := redeclar and reasain not allowed
+*/
+/* 
+prefer not to use var
+cause of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountState,accountCity,accountPassword])
---
+++ datatype/datatype.js
@@ -0,0 +1,30 @@
+
+'use strict' // treat  all js code as newer version
+//alert (4+ 6)  // we are useing node js , not browser
+console.log( 8 + 5) ; console.log('rakib')//code readibility should be high
+console.log(6 
+    + 2) // code readibility should be high
+    //=======================
+    //recomandent documentation mdn 
+    //official documentation ecma script
+    // datatype
+    let name = 'rakib'
+    let age = 18
+    let isLoggIn = false
+
+    // primitive datatype
+    // number => 2 to power 53
+    // bigint
+    // string = ""
+    // boolean => true/false
+    // null => standalone value
+    // undefine => value not assign
+    // symble => unique
+
+    console.log(typeof name)
+    console.log(typeof age)
+    console.log(typeof isLoggIn)
+    console.log(typeof null) // object 
+    console.log(typeof undefined) // undefine
+    
+    // non pro
